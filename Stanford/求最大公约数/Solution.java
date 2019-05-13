9e5c31d0a057deddad9180cd2ea476756f4c94f6
chenyongda2018 <1243724041@qq.com>
2019-05-13 22:58:35 +0800

    求最大公约数

---
+++ Stanford/求最大公约数/Solution.java
@@ -0,0 +1,22 @@
+package 求最大公约数;
+
+public class Solution {
+
+    public static int gdc(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        if (b == 0) {
+            return a;
+        }
+        if (a >= b) {
+            return gdc(a - b, b);
+        } else {
+            return gdc(a, b - a);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gdc(8,48 ));
+    }
+}
