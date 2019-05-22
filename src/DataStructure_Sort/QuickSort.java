150b3fe0c9f225c6f22ac97f37f0a56a390ad75e
chenyongda2018 <1243724041@qq.com>
2019-05-22 10:22:53 +0800

    add QuickSort

--- src/DataStructure_Sort/QuickSort.java
+++ src/DataStructure_Sort/QuickSort.java
@@ -6,64 +6,53 @@ public class QuickSort {
 
     public void quickSort(int[] data, int length, int start, int end) {
         if (data == null || length <= 0 || start < 0 || end >= length) {
-            return ;
+            return;
         }
-        if ( start == end) {
+        if (start == end) {
             return;
         }
         int index = partition(data, length, start, end);
         if (index > start) {
-            quickSort(data, length, start, index-1);
+            quickSort(data, length, start, index - 1);
         }
-        if (index < end ){
-            quickSort(data, length, index+1,end );
+        if (index < end) {
+            quickSort(data, length, index + 1, end);
         }
     }
+
     public int partition(int[] data, int length, int start, int end) {
         if (data == null || length <= 0 || start < 0 || end >= length) {
             return -1;
         }
         int index = 0;
         int small = start - 1;
+        int temp = 0;
         for (index = start; index <= end; index++) {
 
             if (data[index] < data[end]) {
                 small++;
                 if (small != index) {
-                    swap(data[index], data[small]);
+
+                    temp = data[index];
+                    data[index] = data[small];
+                    data[small] = temp;
                 }
             }
         }
         small++;
-        swap(data[small], data[end]);
+        temp = data[small];
+        data[small] = data[end];
+        data[end] = temp;
         return small;
     }
 
-    public void swap(int a, int b) {
-        int sum = a +b ;
-        a = sum - a;
-        b =  sum - a;
-    }
-
-
     public static void main(String[] args) {
-//        int[] data = {2,3,4,5,6,2,12,5,6};
+        int[] data = {5,4,3,2,7,8,4,1};
         QuickSort sort = new QuickSort();
-//        sort.quickSort(data, data.length, 0, data.length-1);
-//        for (int i = 0; i < data.length; i++) {
-//            System.out.print( data[i]+" ");
-//        }
-//        int a =1;
-//        int b = 2;
-//        sort.swap(a, b);
-        String str = "hello";
-        sort.foo(str);
-        System.out.println(str);
-
-    }
-
-    void foo(String str) {
-        str = "123";
+        sort.quickSort(data, data.length, 0, data.length-1);
+        for (int i = 0; i < data.length; i++) {
+            System.out.print( data[i]+" ");
+        }
     }
 
 }
