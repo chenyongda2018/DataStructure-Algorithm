3f3e6e0444b327c703a10df6d4a7ad3a21e9ace7
chenyongda2018 <1243724041@qq.com>
2019-05-26 17:07:49 +0800

    add 删除链表的节点

---
+++ 剑指Offer/T_18_删除链表的节点/Solution.java
@@ -0,0 +1,31 @@
+package T_18_删除链表的节点;
+
+class ListNode {
+    int val;
+    ListNode pNext;
+}
+
+public class Solution {
+    public void deleteNode(ListNode pHead, ListNode pDelete) {
+        if (pHead == null || pDelete == null) {
+            return;
+        }
+        //如果不是尾节点，则把此节点的下一节点的值赋给这个要被删除的节点，将下一节点赋予null
+        if (pDelete.pNext != null) {
+            pDelete.val = pDelete.pNext.val;
+            pDelete.pNext = pDelete.pNext.pNext;
+        } else if (pDelete == pHead) {//如果是头节点，而且是尾节点，则直接赋予null
+            pDelete = null;
+        } else { //如果是尾节点，而且前面有其他节点
+            ListNode node = pHead;
+            while (node.pNext != null) {
+                if (node.pNext == pDelete) {
+                    break;
+                }
+                node = node.pNext;
+            }
+            node.pNext = null;
+        }
+
+    }
+}
