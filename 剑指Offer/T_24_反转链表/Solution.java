7d3fd27ba27217594a2db2574606f675bbdbc09c
chenyongda2018 <1243724041@qq.com>
2019-06-03 17:43:21 +0800

    add 反转链表

---
+++ 剑指Offer/T_24_反转链表/Solution.java
@@ -0,0 +1,30 @@
+package T_24_反转链表;
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode reverseHead = null;
+        ListNode node = head;
+        ListNode pre = null;
+        while (node != null) {
+            ListNode next = node.next; //先保存正常顺序下结点的next结点
+            if (next == null) {
+                reverseHead = node; //遍历到正序的尾部，尾结点作为反转后的头节点
+            }
+            node.next = pre; //调整结点的next方向
+            //指针后移，继续遍历
+            pre = node;
+            node = next;
+        }
+        return reverseHead;
+    }
+}
