78075ae4c033a65d8626ff574eb46db6982c9ea9
chenyongda2018 <1243724041@qq.com>
2019-06-03 17:14:48 +0800

    add 链表中环的入口结点

--- 剑指Offer/T_23_链表中环的入口结点/Solution.java
+++ 剑指Offer/T_23_链表中环的入口结点/Solution.java
@@ -14,6 +14,56 @@ class ListNode {
 public class Solution {
 
     public ListNode EntryNodeOfLoop(ListNode pHead) {
+        ListNode meetingNode = meetingNode(pHead);
+        if (meetingNode == null) {
+            return null;//不存在环
+        }
+        int loopNodeCount = 1;
+        ListNode node = meetingNode;
+        while (node.next != meetingNode) {
+            node = node.next;
+            loopNodeCount++; //记录环节点的个数
+        }
+
+        //找出环的入口结点,先让第一个结点走n(环结点个数)步，然后两个结点再每次都走一步，相遇的结点就是入口结点
+        node = pHead;
+        for (int i = 0; i <loopNodeCount ; i++) {
+            node = node.next;
+        }
+        ListNode node2 = pHead;
+        while (node != node2) {
+            node = node.next;
+            node2 = node2.next;
+        }
+        return node;
+    }
+
+    /**
+     * 在有环的情况下找出两指针相遇的结点(此结点在环中)
+     * 不存在环时返回null
+     * @param pHead
+     * @return
+     */
+    public ListNode meetingNode(ListNode pHead) {
+        if (pHead == null) {
+            return null;
+        }
+        ListNode pSlow = pHead;
+        if (pSlow == null) {
+            return null;
+        }
+        ListNode pQuick = pSlow.next;
+        while (pSlow != null && pQuick != null) {
+            if (pSlow == pQuick) {
+                return pQuick;
+            }
+
+            pSlow = pSlow.next;
+            pQuick = pQuick.next;
+            if (pQuick != null) {
+                pQuick = pQuick.next;
+            }
+        }
         return null;
     }
 }
