27f4140b8d9eec541386ff7d8e7414d4b758cd49
chenyongda2018 <1243724041@qq.com>
2019-06-09 16:53:28 +0800

    update README.md

---
+++ 剑指Offer/T_32_3_之字形印二叉树/Solution.java
@@ -0,0 +1,15 @@
+package T_32_3_之字形印二叉树;
+
+import java.util.ArrayList;
+
+import DataStructure_Tree.TreeNode;
+
+/**
+ * 题目描述
+ * 请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+ */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+
+    }
+}
