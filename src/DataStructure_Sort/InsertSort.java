e23b8bf99fd13120c056872405ede573266b610b
chenyongda2018 <1243724041@qq.com>
2019-06-20 15:50:48 +0800

    add 最小的k个数

--- src/DataStructure_Sort/InsertSort.java
+++ src/DataStructure_Sort/InsertSort.java
@@ -2,10 +2,10 @@ package DataStructure_Sort;
 
 public class InsertSort {
 
-    public int[] insertSort(int[] a) {
+    public int[] insertSort(int[] a,int start,int end) {
         if (a.length > 1) {
             int j = 0;
-            for (int i = 1; i < a.length; i++) {
+            for (int i = start+1; i <=end; i++) {
                 int num = a[i];
                 for (j = i; j > 0 && a[j - 1] > num; j--) {
                     a[j] = a[j - 1];
@@ -19,7 +19,7 @@ public class InsertSort {
     public static void main(String[] args) {
         InsertSort insertSort = new InsertSort();
         int[] a = new int[]{1, 2, 3, 35, 23, 67, 34, 89, 12, 34};
-        int[] sort_a = insertSort.insertSort(a);
+        int[] sort_a = insertSort.insertSort(a,0,a.length-1);
 
         for (int num : sort_a) {
             System.out.println(num + " ");
--- src/DataStructure_Sort/QuickSort.java
+++ src/DataStructure_Sort/QuickSort.java
@@ -8,8 +8,13 @@ public class QuickSort {
         if (data == null || length <= 0 || start < 0 || end >= length) {
             return;
         }
+
         if (start == end) {
             return;
+        }
+        if (start + end == 8) { //当子数列规模降到8时采用插入排序
+            System.out.println("insert sort");
+            insertSort(data, start, end);
         }
         int index = partition(data, length, start, end);
 
@@ -27,27 +32,49 @@ public class QuickSort {
         }
         int index = 0;
         int small = start - 1;
-        int temp = 0;
         for (index = start; index <= end; index++) {
-
             if (data[index] < data[end]) {
                 small++;
                 if (small != index) {
-                    temp = data[index];
-                    data[index] = data[small];
-                    data[small] = temp;
+                    swap(data,small ,index );
                 }
             }
         }
         small++;
-        temp = data[small];
-        data[small] = data[end];
-        data[end] = temp;
+        swap(data,small ,end );
         return small;
     }
 
+    public void swap(int[] data,int a,int b){
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
+    }
+
+    public void insertSort(int[] a,int start,int end) {
+        if (a.length > 1) {
+            int j = 0;
+            for (int i = start+1; i <=end; i++) {
+                int num = a[i];
+                for (j = i; j > 0 && a[j - 1] > num; j--) {
+                    a[j] = a[j - 1];
+                }
+                a[j] = num;
+            }
+        }
+    }
+
+    public void shuffle(int[] data,int start,int end) {
+        if(start < end) {
+            for (int i = 0; i <= end ; i++) {
+                int r = (int)Math.random()*(end-i);
+                swap(data,r ,i );
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int[] data = {5,4,3,2,7,8,4,1};
+        int[] data = {5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1,5,4,3,2,7,8,4,1};
         QuickSort sort = new QuickSort();
         sort.quickSort(data, data.length, 0, data.length-1);
         for (int i = 0; i < data.length; i++) {
