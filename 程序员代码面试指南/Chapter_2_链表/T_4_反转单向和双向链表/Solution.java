d5274d44877bd9a134b12e14f3c0cb8fd9e64362
chenyongda2018 <1243724041@qq.com>
2019-07-15 14:04:47 +0800

    add 反转双向链表

--- 程序员代码面试指南/Chapter_2_链表/T_4_反转单向和双向链表/Solution.java
+++ 程序员代码面试指南/Chapter_2_链表/T_4_反转单向和双向链表/Solution.java
@@ -2,18 +2,46 @@ package Chapter_2_链表.T_4_反转单向和双向链表;
 
 public class Solution {
 
+    /**
+     * 反转单项链表
+     *
+     * @param head
+     * @return
+     */
     public static Node reverseNode(Node head) {
         if (head == null) return head;
         Node pre = null;
         Node next = null;
-        while(head != null) {
+        while (head != null) {
             next = head.next;
             head.next = pre;
             pre = head;
             head = next;
         }
-        return head;
+        return pre;
 
+    }
+
+
+    /**
+     * 反转双向链表
+     * @param head
+     * @return
+     */
+    public static DoubleNode reverseDoubleNode(DoubleNode head) {
+
+        if (head == null) return head;
+
+        DoubleNode pre = null;
+        DoubleNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.last = next;
+            head.next = pre;
+            pre = head;
+        }
+
+        return pre;
     }
 
 }
@@ -26,4 +54,15 @@ class Node {
     public Node(int val) {
         this.val = val;
     }
+}
+
+class DoubleNode {
+    int val;
+    DoubleNode next;
+    DoubleNode last;
+
+    DoubleNode(int val) {
+        this.val = val;
+    }
+
 }
