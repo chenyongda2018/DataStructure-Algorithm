e4f6df9b0a4019a235644635e5b767d8c2097a42
chenyongda2018 <1243724041@qq.com>
2019-07-30 08:44:58 +0800

    add 复制含有随机指针的链表

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,29 @@
+import java.util.concurrent.*;
+
 /**
  * Created by cyd on 19-4-8.
  */
 
 public class Test {
     public static void main(String[] args) {
-        ClassLoader c = Test.class.getClassLoader();
-        while(c != null) {
-            System.out.println(c.toString());
-            c = c.getParent();
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+        Future<Integer> task = threadPool.submit(new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                Thread.sleep(2000);
+                return 1;
+            }
+        });
+        System.out.println("主线程在执行");
+        try {
+            if(task.get() .equals(1)) {
+                System.out.println("task执行结束");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
     }
 }
--- 程序员代码面试指南/Chapter_2_链表/T_11_复制含有随机指针结点的链表/Solution.java
+++ 程序员代码面试指南/Chapter_2_链表/T_11_复制含有随机指针结点的链表/Solution.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 
 public class Solution {
 
+    //方法一 : 通过哈希表存储原结点,副本结点
     public Node copyListWithRand(Node head) {
         //用哈希表存储<原结点,副本结点>
         HashMap<Node,Node> map = new HashMap<>();
@@ -22,6 +23,42 @@ public class Solution {
             map.get(cur).rand = cur.rand;
         }
         return map.get(head);
+    }
+
+    //时间换空间
+    public Node copyListWithRand_2(Node head) {
+
+        if(head == null) return null;
+
+        Node cur = head;
+        Node next = null;
+        //创建副本结点
+        while(cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.val);
+            cur.next.next = next;
+            cur = next;
+        }
+        //为副本结点的rand域赋值
+        cur = head;
+        while(cur!=null) {
+            cur.next.rand = cur.rand == null ? null : cur.rand.next;
+            cur = cur.next.next;
+        }
+
+        cur = head;
+        Node res = cur.next;
+
+        while(cur != null) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = cur.next.next;
+            cur = cur.next;
+            next = next.next;
+        }
+
+        return res;
+
 
     }
 }
