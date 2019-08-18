11e7142b87f8aa42447b3af808d4c93ea28264b2
chenyongda2018 <1243724041@qq.com>
2019-08-18 22:14:16 +0800

    add 反转句子

--- 剑指Offer/T_57_1_和为S的序列/Solution.java
+++ 剑指Offer/T_57_1_和为S的序列/Solution.java
@@ -3,11 +3,12 @@ package T_57_1_和为S的序列;
 import java.util.ArrayList;
 public class Solution {
     public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         int left = 1;
         int right = 2;
         while(left < right) {
-            int cur = ((left+right)*(right - left + 1))/2;
+            int cur = ((left+right)*(right - left + 1))/2; //求和公式
             if(cur == sum) {
                 ArrayList<Integer> list = new ArrayList<>();
                 for(int i = left ; i <= right;i++){
@@ -15,13 +16,11 @@ public class Solution {
                 }
                 res.add(list);
                 left++;
-            } else if(cur < sum) {
+            } else if(cur < sum) { //如果当前序列和小于sum，则需要增大，right右移
                 right++;
-            } else {
+            } else {   //如果当前序列和大于sum，则需要减小，left右移
                 left++;
             }
-
-
         }
         return res;
 
---
+++ 剑指Offer/T_58_反转句子/Solution.java
@@ -0,0 +1,23 @@
+package T_58_反转句子;
+
+public class Solution {
+
+    public String ReverseSentence(String str) {
+        if(str == null||str.trim().equals("")) return str;
+        String[] arr = str.split(" ");
+        int len = arr.length-1;
+        int i = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int j = len; j >=0 ; j--) {
+
+            if( j == 0) { //最后一个单词不加空格
+                sb.append(arr[j]);
+            }else {
+                sb.append(arr[j]).append(" ");//其他单词加空格
+            }
+
+        }
+        return sb.toString();
+    }
+}
