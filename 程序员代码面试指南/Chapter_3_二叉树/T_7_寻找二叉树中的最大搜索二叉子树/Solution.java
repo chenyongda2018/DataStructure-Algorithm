26fdff7cc5a3af370fa84975fe3e3ef374ec15ab
chenyongda2018 <1243724041@qq.com>
2019-08-23 23:08:11 +0800

    add 寻找二叉树中的最大搜索二叉子树

--- 程序员代码面试指南/Chapter_3_二叉树/T_7_寻找二叉树中的最大搜索二叉子树/Solution.java
+++ 程序员代码面试指南/Chapter_3_二叉树/T_7_寻找二叉树中的最大搜索二叉子树/Solution.java
@@ -11,22 +11,22 @@ public class Solution {
 
     public ReturnType getProcess(TreeNode head) {
         if(head == null) return new ReturnType(null,0,Integer.MIN_VALUE,Integer.MAX_VALUE);
-        ReturnType lNode = getProcess(head.left);
-        ReturnType rNode = getProcess(head.right);
+        ReturnType lNode = getProcess(head.left);//获取左子树的信息
+        ReturnType rNode = getProcess(head.right); //获取右子树的信息
 
-        int min = Math.min(lNode.min,Math.min(head.val,rNode.min));
-        int max = Math.max(rNode.max,Math.max(head.val,rNode.max));
+        int min = Math.min(lNode.min,Math.min(head.val,rNode.min)); //获取以当前节点为整树的最小值
+        int max = Math.max(rNode.max,Math.max(head.val,rNode.max)); //获取以当前节点为整树的最大值
 
-        int maxBSDSize = Math.max(lNode.maxBSDSize,rNode.maxBSDSize);
-        TreeNode maxHead = lNode.maxBSDSize >= rNode.maxBSDSize ? lNode.maxHead : rNode.maxHead;
+        int maxBSDSize = Math.max(lNode.maxBSDSize,rNode.maxBSDSize); //获取左右子树二叉搜索子树最大的
+        TreeNode maxHead = lNode.maxBSDSize >= rNode.maxBSDSize ? lNode.maxHead : rNode.maxHead;//二叉搜索子树的头节点
 
         if(lNode.maxHead == head.left && rNode.maxHead == head.right
                 && head.val < rNode.min && head.val > lNode.max) {
-            maxHead = head;
+            maxHead = head; //如果满足条件，此节点本身就是以此节点为整树的最大二叉搜索子树头节点
             maxBSDSize = lNode.maxBSDSize + rNode.maxBSDSize;
         }
 
-        return new ReturnType(maxHead,maxBSDSize,max,min);
+        return new ReturnType(maxHead,maxBSDSize,max,min);//整合信息返回
     }
 }
 
