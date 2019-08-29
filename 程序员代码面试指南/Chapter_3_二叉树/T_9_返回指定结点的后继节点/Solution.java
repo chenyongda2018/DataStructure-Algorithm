9f447898cd6145cad59831d024a29daa7880296d
chenyongda2018 <1243724041@qq.com>
2019-08-29 23:12:18 +0800

    add binary search

--- 程序员代码面试指南/Chapter_3_二叉树/T_9_返回指定结点的后继节点/Solution.java
+++ 程序员代码面试指南/Chapter_3_二叉树/T_9_返回指定结点的后继节点/Solution.java
@@ -1,4 +1,5 @@
 package Chapter_3_二叉树.T_9_返回指定结点的后继节点;
+
 class TreeLinkNode {
     int val;
     TreeLinkNode left = null;
@@ -9,18 +10,18 @@ class TreeLinkNode {
         this.val = val;
     }
 }
+
 public class Solution {
     //思路 : 若结点有右子树，则返回右子树的最左结点
     //若没有右子树，判断此节点是不是父节点的左孩子，是的话，返回父节点，不是的话，一直往上找是父节点的左孩子的结点。返回结点的父节点
 
-    public TreeLinkNode GetNext(TreeLinkNode pNode)
-    {
-        if(pNode == null) return null;
-        if(pNode.right!=null) {
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode == null) return null;
+        if (pNode.right != null) {
             return getMostLeft(pNode.right);
         } else {
             TreeLinkNode p = pNode.next;
-            while(p!=null && p.left!=pNode) {
+            while (p != null && p.left != pNode) {
                 pNode = p;
                 p = pNode.next;
             }
@@ -29,12 +30,10 @@ public class Solution {
     }
 
     public TreeLinkNode getMostLeft(TreeLinkNode node) {
-        if(node== null) return node;
-        while(node.left!=null) node = node.left;
+        if (node == null) return node;
+        while (node.left != null) node = node.left;
         return node;
     }
-
-
 
 
 }
